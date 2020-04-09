0b1293b5f82a13b7f9631820d6afc2930565b369
Diego Cabral <diegopgc@gmail.com>
2020-04-09 14:12:54 -0300

    Add front-end com ReactJS

---
+++ front-end-reactjs/README.md
@@ -0,0 +1,20 @@
+<img alt="GoStack" src="https://storage.googleapis.com/golden-wind/bootcamp-gostack/header-desafios.png" />
+<h2 align="center">
+  Front-end com ReactJS
+</h2>
+
+## Assuntos abordados:
+
+1. Conceitos ReactJS
+2. Configurando Babel
+3. Configurando WebPack
+4. Componentização
+5. Propriedades
+6. Estado e Imutabilidade
+7. Importando CSS e imagens
+8. Listando projetos da API
+9. Cadastrando projetos
+
+---
+
+Feito com 💜 by Diego Cabral :wave: [Linkedin](https://www.linkedin.com/in/diego-pg-cabral/)
---
+++ front-end-reactjs/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+
+import './App.css';
+
+import Header from './components/Header';
+import api from './services/api';
+
+export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    })
+  }, []);
+
+  async function handleAddProject() {
+    const response = await api.post('/projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: 'Diego Cabral',
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  } 
+
+  return (
+    <>
+      <Header title="Projects"/>
+
+      <ul>
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Adicionar projetos</button>
+    </>
+  )
+}
